bf01638af24eb2204eb389c5be3e4f1b0000a869
Administrator <Administrator@USER-20170829SB>
2018-04-10 11:43:59 +0800

    opt some configration

--- src/main/java/com/mgs/controller/MemberController.java
+++ src/main/java/com/mgs/controller/MemberController.java
@@ -7,19 +7,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mgs.entity.Plog;
 import com.mgs.entity.Pmember;
+import com.mgs.service.PlogService;
 import com.mgs.service.PmemberService;
 
 @Controller
 public class MemberController {
-	
+
 	@Autowired
 	private PmemberService pmemberService;
-	
-	@RequestMapping("/")
+
+	@Autowired
+	private PlogService plogService;
+
+	@RequestMapping("/pms")
 	@ResponseBody
-	public List<Pmember> viewALL(){
+	public List<Pmember> viewALL() {
 		return pmemberService.getMembersByCondition("");
+	}
+
+	@RequestMapping("/aixes")
+	@ResponseBody
+	public List<Plog> viewAixes() {
+		return plogService.queryAllLogsInDays();
 	}
 
 }
